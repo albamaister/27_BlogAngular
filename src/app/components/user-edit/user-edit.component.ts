3f26ccdea17371c0c81e71848e1f5a080ea05bdb
Andres Alba <bryanalba92@gmail.com>
2019-10-13 00:19:48 -0500

    Actualización del usuario

--- src/app/components/user-edit/user-edit.component.ts
+++ src/app/components/user-edit/user-edit.component.ts
@@ -13,21 +13,63 @@ export class UserEditComponent implements OnInit {
   public user: User;
   public identity;
   public token;
+  public status;
 
   constructor( private _userService: UserService ) {
     this.page_title = 'Ajustes de usuario';
-    this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '');
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
     // Rellenar objeto usuario
-    this.user = this.identity;
+    this.user = new User(this.identity.sub,
+                         this.identity.name,
+                         this.identity.surname,
+                         this.identity.role,
+                         this.identity.email,
+                         '',
+                         this.identity.description,
+                         this.identity.image);
    }
 
   ngOnInit() {
   }
 
-  onSubmit() {
+  onSubmit(form) {
+    this._userService.update(this.token, this.user).subscribe(
+        response => {
+          if ( response ) {
+            this.status = 'success';
+            console.log(response);
+            // Actualizar uduario en sesion
+            if ( response.changes.name ) {
+              this.user.name = response.changes.name;
+            }
 
+            if ( response.changes.surname ) {
+              this.user.surname = response.changes.surname;
+            }
+
+            if ( response.changes.email ) {
+              this.user.email = response.changes.email;
+            }
+
+            if ( response.changes.description ) {
+              this.user.description = response.changes.description;
+            }
+
+            if ( response.changes.image ) {
+              this.user.image = response.changes.image;
+            }
+            this.identity = this.user;
+            localStorage.setItem('identity', JSON.stringify(this.identity))
+          } else {
+            this.status = 'error';
+
+          }
+        },
+        error => {
+          this.status = error;
+        }
+    );
   }
 
 }
